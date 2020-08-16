73855e6e45894cd1dd43077aa110f55d85ec870a
Harishkumar <harishkumar.cherla@gmail.com>
2020-08-16 18:00:04 +0530

    Variables

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ console.log("Hello World!");
     TODO: Name should be dunamic
 */
 console.log("Hello World2!");
+
+//variables
+var message;
+
+//assignment
+//message = prompt("Enter your message: ");
+
+//reading
+console.log(message);
+
+var name = "Harish";
+var age = 32;
+
+name = "Krishna";
+age = 27;
+
+console.log("Name: " + name + ", Age: " + age);
