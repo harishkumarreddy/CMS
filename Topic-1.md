7c0cb7e30ae7961d9912baba2f8c2e1cfcda1583
Harishkumar Reddy <harishkumarreddy.cherla@gmail.com>
2020-08-22 17:59:43 +0530

    DataTyps, Variables and Operators

--- Topic-1.md
+++ Topic-1.md
@@ -1,1 +1,30 @@
-# DataTyps, Variables and decissionmaking
+[Home](Readme.md)
+# DataTyps, Variables and Operators 
+
+## Variables:
+    Variablesare memory blocks to store and retreave data/valueas by using a naming convention
+    **Syntax**
+    > var myName = "Harishkumar";
+
+## DataTyps:
+    ### Pramethive Data typs
+    1. Staring -> alpha Numaric data(A-Z,a-z,0-9 and any special chars)
+    1. Intiger -> 0-9
+    1. flot -> 10.4568
+    1. datetime -> date and time 
+
+    ### Scalable Datata type
+    #### Array:
+
+## Operators:
+ 1. Assignment operatores -> =
+ 1. Math operators -> +,-,*,/,%
+ 1. Comparation -> ==,!=, <, <=, >. >=
+ 1. Binary/ Operators (X) -> ->. <- 
+
+1-9,A-F -> d4f4c2
+ 0000 = 0
+ 0001 = 1
+ 0010 = 2
+ ...
+ 1111 = F
---
+++ topc-1.js
@@ -0,0 +1,52 @@
+// variables and data typs
+var myName = "Harishkumar";
+var age = 33;
+var height = 5.9;
+var email = "harishlimarreddy.cherla@gmail.com";
+var DoB = new Date('sep-02-1987');
+
+// Reading values and concatinaing 
+console.log("Name: " + myName);
+console.log("Age: " + age);
+console.log("Height: " + height);
+console.log("Email: " + email);
+console.log("DOB: " + DoB);
+
+// Operators
+var A;
+var B;
+
+A = 3;
+B = 2;
+var C = A+B;
+
+console.log("Add: " + (A+B));
+console.log("Sub: " + (A-B));
+console.log("Mul: " + (A*B));
+console.log("Dev: " + (A/B));
+console.log("Mod: " + (A%B));
+
+// ================================
+/* 
+    a = 2
+    b = 3
+   =======
+    a = 3
+    b = 2
+ */
+
+ var a = -10;
+ var b = 0;
+
+ var c = a;
+ a=b;
+ b=c;
+
+ console.log("a : " + a);
+ console.log("b : " + b);
+         //  a  b
+a = a+b; // 200+30 = 230;
+b = a-b; // 230-30 = 200;
+a = a-b; // 230-200 = 30;
+
+//
